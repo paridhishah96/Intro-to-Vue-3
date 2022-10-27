f71a53d5bcfa6a38b3b54694a0253d37d9eb364f
paridhishah96 <paridhishah96@gmail.com>
2022-10-27 16:44:24 -0400

    completed l9 exercise

---
+++ components/ProductDetails.js
@@ -0,0 +1,18 @@
+app.component("product-details", {
+    props:{
+        premium: {
+            type: Array,
+            required: true
+        }
+    },
+    template:
+    /*html*/
+    `<ul>
+        <li v-for="detail in details">{{ detail }}</li>
+    </ul>`,
+    data() {
+        return {
+            details: ['50% cotton', '30% wool', '20% polyester'],
+        }
+    }
+})
